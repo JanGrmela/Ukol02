47d9b866003877d8890f0d0feea59bbd4df3b46a
Jan Grmela <j.grmela@volny.cz>
2024-05-28 10:25:12 +0200

    Create class BookingManager

---
+++ src/com/engeto/BookingManager.java
@@ -0,0 +1,29 @@
+package com.engeto;
+
+import java.util.List;
+
+public class BookingManager {
+    String ListOfBooking;
+    int index;
+
+//Vložení rezervace do seznamu
+    public void addBooking(Booking newBooking) {
+            bookingsList.add(newBooking);
+
+}
+
+// Získání rezervace se zadaným indexem ze seznamu
+    public class bookingList {
+        public static Booking getBookingByIndex(List<Booking>bookingList, int index) {
+            if (index >= 0 && index < bookingList.size()) {
+                return bookingList.get(index);
+            }
+            return null;
+        }
+    }
+// Získání seznamu rezervací
+    List<Reservation> reservations = reservationService.getAllReservations();
+
+//Vymazání seznamu rezervací:
+    List<Reservation> reservations = new ArrayList<>();
+    reservations.clear();
