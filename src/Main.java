898af86a3f7ee210c3822a7d63c29b81f33ce0d0
Jan Grmela <j.grmela@volny.cz>
2024-05-30 15:22:20 +0200

    set up Reservations

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,8 @@ public class Main {
 
 
         public static void main(String[] args) {
+
 // Hosté dle zadání
-
-
                 GuestsOfHotel firstguest = new GuestsOfHotel("Adéla", "Malíková", LocalDate.of(1993, 3, 13));
                 GuestsOfHotel secondguest = new GuestsOfHotel("Jan", "Dvořáček", LocalDate.of(1995, 05, 05));
 
@@ -18,32 +17,23 @@ public class Main {
                 System.out.println(secondguest.getName() + " " + secondguest.getSurname() + " " + "(" + secondguest.getDateOfBirth() + ")");
 
 // Evidence pokojů
-                Room first = new Room(01, 1, true, true, 1000);
-                Room second = new Room(02, 1, true, true, 1000);
-                Room third = new Room(03, 3, false, true, 2400);
-
-                System.out.println("číslo pokoje:" + first.getroomOfNumber() + "počet lůžek:" + first.getNumberOfBeds() + "cena za noc:" + first.getPricePerNight() + "s balkónem:" + first.isHasBalcony() + "výhled na moře:" + first.isHasSeaView());
-                System.out.println("číslo pokoje:" + second.getRoomOfNumber() + "počet lůžek:" + second.getNumberOfBeds() + "cena za noc:" + second.getPricePerNight() + "s balkónem:" + second.isHasBalcony() + "výhled na moře:" + second.isHasSeaView());
-                System.out.println("číslo pokoje:" + third.getRoomOfNumber() + "počet lůžek:" + third.getNumberOfBeds() + "cena za noc:" + third.getPricePerNight() + "s balkónem:" + third.isHasBalcony() + "výhled na moře:" + third.isHasSeaView());
+                Room first = new Room(1, 1, true, true, 1000);
+                Room second = new Room(2, 1, true, true, 1000);
+                Room third = new Room(3, 3, false, true, 2400);
+                System.out.println("Evidence pokojů :");
+                System.out.println("číslo pokoje:" + first.getNumberOfRoom()+"; " + "počet lůžek:" + first.getNumberOfBeds() +"; " +"cena za noc:" + first.getPricePerNight() +"; "+ "s balkónem:" + first.isHasBalcony() +"; "+ "výhled na moře:" + first.isHasSeaView());
+                System.out.println("číslo pokoje:" + second.getNumberOfRoom() +"; "+ "počet lůžek:" + second.getNumberOfBeds() +"; " +"cena za noc:" + second.getPricePerNight() +"; "+ "s balkónem:" + second.isHasBalcony() +"; "+ "výhled na moře:" + second.isHasSeaView());
+                System.out.println("číslo pokoje:" + third.getNumberOfRoom() +"; "+ "počet lůžek:" + third.getNumberOfBeds() +"; "+ "cena za noc:" + third.getPricePerNight() +"; "+ "s balkónem:" + third.isHasBalcony() +"; "+ "výhled na moře:" + third.isHasSeaView());
 
 // Reservace dle zadání
-                Reservation reservationFirst = new Reservation(01,"Adela Malíková","19.07.21","26.07.21",false);
-                Reservation reservationSecond = new Reservation(03,"Adela Malíková,Jan Dvořáček","01.09.21","14.09.21",false)
-// Seznam všech reservací
+                Reservation reservationFirst = new Reservation(1,"Adela Malíková","19.07.21","26.07.21",false);
+                Reservation reservationSecond = new Reservation(3,"Adela Malíková,Jan Dvořáček","01.09.21","14.09.21",false);
+
+                System.out.println("Reservation Details:");
+                System.out.println("První reservace: " + "pokoj č.: "+reservationFirst.getRoomOfNumber()+" ;host: "+ reservationFirst.getGuestsOfHotel() +" ;příjezd: "+reservationFirst.getCheckInDate()+" ;odjezd: "+reservationFirst.getCheckOutDate()+" ;typ pobytu: "+reservationFirst.isStayType());
+                System.out.println("Druhá reservace: " + "pokoj č.: "+reservationSecond.getRoomOfNumber()+" ;host: "+ reservationSecond.getGuestsOfHotel() +" ;příjezd: "+reservationSecond.getCheckInDate()+" ;odjezd: "+reservationSecond.getCheckOutDate()+" ;typ pobytu: "+reservationSecond.isStayType());
 
 
-
-                                String guestsOfHotels = "Adéla";
-                                int roomNumber = 1;
-                                String checkInDate = "19th July 2021";
-                                String checkOutDate = "26th July 2021";
-
-                                System.out.println("Reservation Details:");
-                                System.out.println("Guest Name: " + guestsOfHotels);
-                                System.out.println("Room Number: " + roomNumber);
-                                System.out.println("Check-in Date: " + checkInDate);
-                                System.out.println("Check-out Date: " + checkOutDate);
-                        }
                 }
 
 
@@ -64,7 +54,7 @@ public class Main {
         //        reservationList.remove(0);
         //        for (Reservation reservation : reservationList) {
         //        System.out.println("All Reservations:" + reservationList);
-                }
+
 
 
         }
@@ -72,7 +62,7 @@ public class Main {
 
 
 
-}
+
 
 
 
