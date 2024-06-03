f72ebbd50e4cbab79af03831f778df60ec0c5c68
Jan Grmela <j.grmela@volny.cz>
2024-06-03 11:34:21 +0200

    List of guests

--- src/com/engeto/Reservation.java
+++ src/com/engeto/Reservation.java
@@ -1,6 +1,7 @@
 package com.engeto;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class Reservation {
         private int roomOfNumber;
@@ -10,65 +11,105 @@ public class Reservation {
         private boolean stayType;
         private String guestsOfHotelList;
         private String roomList;
-
-
+        private Room room;
+        private GuestsOfHotel guestsOfHotel1;
+        private List<GuestsOfHotel> otherGuests;
 
 
         public Reservation(int roomOfNumber, String guestsOfHotel, LocalDate checkInDate, LocalDate checkOutDate, boolean stayType, String guestsOfHotelList, String roomList) {
-            this.roomOfNumber = roomOfNumber;
-            this.guestsOfHotel = guestsOfHotel;
-            this.checkInDate = checkInDate;
-            this.checkOutDate = checkOutDate;
-            this.stayType = stayType;
-            this.guestsOfHotelList = guestsOfHotelList;
-            this.roomList = roomList;
+                this.roomOfNumber = roomOfNumber;
+                this.guestsOfHotel = guestsOfHotel;
+                this.checkInDate = checkInDate;
+                this.checkOutDate = checkOutDate;
+                this.stayType = stayType;
+                this.guestsOfHotelList = guestsOfHotelList;
+                this.roomList = roomList;
 
-        }
-        public String getGuestsOfHotelList() {
-            return guestsOfHotelList;
-        }
-        public void setGuestsOfHotelList(String guestsOfHotelList) {
-            this.guestsOfHotelList = guestsOfHotelList;
-        }
-        public String getRoomList() {
-            return roomList;
-        }
-        public void setRoomList(String roomList) {
-            this.roomList = roomList;
+
+
         }
 
         public int getRoomOfNumber() {
-            return roomOfNumber;
-        }
-        public void setRoomOfNumber(int roomOfNumber) {
-            this.roomOfNumber = roomOfNumber;
-        }
-        public String getGuestsOfHotel() {
-            return guestsOfHotel;
-        }
-        public void setGuestsOfHotel(String guestsOfHotel) {
-            this.guestsOfHotel = guestsOfHotel;
-        }
-        public LocalDate getCheckInDate() {
-            return checkInDate;
-        }
-        public void setCheckInDate(LocalDate checkInDate) {
-            this.checkInDate = checkInDate;
-        }
-        public LocalDate getCheckOutDate() {
-            return checkOutDate;
-        }
-        public void setCheckOutDate(LocalDate checkOutDate) {
-            this.checkOutDate = checkOutDate;
-        }
-        public boolean isStayType() {
-            return stayType;
-        }
-        public void setStayType(boolean stayType) {
-            this.stayType = stayType;
+                return roomOfNumber;
         }
 
+        public void setRoomOfNumber(int roomOfNumber) {
+                this.roomOfNumber = roomOfNumber;
         }
+
+        public String getGuestsOfHotel() {
+                return guestsOfHotel;
+        }
+
+        public void setGuestsOfHotel(String guestsOfHotel) {
+                this.guestsOfHotel = guestsOfHotel;
+        }
+
+        public LocalDate getCheckInDate() {
+                return checkInDate;
+        }
+
+        public void setCheckInDate(LocalDate checkInDate) {
+                this.checkInDate = checkInDate;
+        }
+
+        public LocalDate getCheckOutDate() {
+                return checkOutDate;
+        }
+
+        public void setCheckOutDate(LocalDate checkOutDate) {
+                this.checkOutDate = checkOutDate;
+        }
+
+        public boolean isStayType() {
+                return stayType;
+        }
+
+        public void setStayType(boolean stayType) {
+                this.stayType = stayType;
+        }
+
+        public String getGuestsOfHotelList() {
+                return guestsOfHotelList;
+        }
+
+        public void setGuestsOfHotelList(String guestsOfHotelList) {
+                this.guestsOfHotelList = guestsOfHotelList;
+        }
+
+        public String getRoomList() {
+                return roomList;
+        }
+
+        public void setRoomList(String roomList) {
+                this.roomList = roomList;
+        }
+
+        public Room getRoom() {
+                return room;
+        }
+
+        public void setRoom(Room room) {
+                this.room = room;
+        }
+
+        public GuestsOfHotel getGuestsOfHotel1() {
+                return guestsOfHotel1;
+        }
+
+        public void setGuestsOfHotel1(GuestsOfHotel guestsOfHotel1) {
+                this.guestsOfHotel1 = guestsOfHotel1;
+        }
+
+        public List<GuestsOfHotel> getOtherGuests() {
+                return otherGuests;
+        }
+
+        public void setOtherGuests(List<GuestsOfHotel> otherGuests) {
+                this.otherGuests = otherGuests;
+        }
+}
+
 
 
 
